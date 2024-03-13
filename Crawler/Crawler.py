3392fc1a4a7712853737f666e97de4b68b631c9e
felipealmanza <f.almanza4@gmail.com>
2024-03-12 22:04:32 -0600

    Getting dashboards and working on Saved searches. Might refactor this function.

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -5,12 +5,39 @@ CONFIG_FILE = "./config.yaml"
 body = {}
 body["output_mode"] = "json"
 
-params = 
+params = {"count": "0"}
 
-def getDashboards(host, port, user, password):
+def getDashboards(hostname, host, port, user, password, objectList):
     requestDashboards = host + ":" + port + "/servicesNS/-/-/data/ui/views"
-    response = requests.get(url=requestDashboards, auth=(user, password), data=body, verify=False, params=).json()
-    return response
+    response = requests.get(url=requestDashboards, auth=(user, password), data=body, verify=False, params=params).json()
+
+    dashboards = response["entry"]
+
+    for dashboard in dashboards:
+        dbEntry = {"splunk_host" : hostname,
+                   "type": "Dashboard",
+                   "object_info" : dashboard,
+                   "custom_meta_labels" : [],
+                   "custom_classification" : ""
+                   }
+        
+        objectList.append(dbEntry)
+        
+def getSavedSearchesOrReports(hostname, host, port, user, password, objectList):
+    requestDashboards = host + ":" + port + "/servicesNS/-/-/data/ui/views"
+    response = requests.get(url=requestDashboards, auth=(user, password), data=body, verify=False, params=params).json()
+
+    dashboards = response["entry"]
+
+    for dashboard in dashboards:
+        dbEntry = {"splunk_host" : hostname,
+                   "type": "Dashboard",
+                   "object_info" : dashboard,
+                   "custom_meta_labels" : [],
+                   "custom_classification" : ""
+                   }
+        
+        objectList.append(dbEntry)
 
 
 with open(CONFIG_FILE) as configFile:
@@ -20,9 +47,16 @@ with open(CONFIG_FILE) as configFile:
 if(len(servers) < 0):
     raise Exception("There are no servers in the file: " + CONFIG_FILE)
 
-
+objectList = []
 
 for server in servers:
-    response = getDashboards(server['host'], str(server['port']), servers[0]['user'],servers[0]['password'])
+    getDashboards(server["hostname"],
+                                server['host'],
+                                str(server['port']),
+                                servers[0]['user'],
+                                servers[0]['password'],
+                                objectList)
+    
 
-    print(response)
+
+    print(objectList)
