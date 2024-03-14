96f79666c9e2ae4386d5e5a30f10f2749bdc7357
felipealmanza <f.almanza4@gmail.com>
2024-03-13 22:33:17 -0600

    Added getapps and getsavedsearches

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -23,16 +23,35 @@ def getDashboards(hostname, host, port, user, password, objectList):
         
         objectList.append(dbEntry)
         
-def getSavedSearchesOrReports(hostname, host, port, user, password, objectList):
-    requestDashboards = host + ":" + port + "/servicesNS/-/-/data/ui/views"
-    response = requests.get(url=requestDashboards, auth=(user, password), data=body, verify=False, params=params).json()
+def getSavedSearches(hostname, host, port, user, password, objectList):
+    requestSearches = host + ":" + port + "/servicesNS/-/-/saved/searches"
+    response = requests.get(url=requestSearches, auth=(user, password), data=body, verify=False, params=params).json()
 
-    dashboards = response["entry"]
+    savedSearches = response["entry"]
 
-    for dashboard in dashboards:
+    for savedSearch in savedSearches:
         dbEntry = {"splunk_host" : hostname,
                    "type": "Dashboard",
-                   "object_info" : dashboard,
+                   "object_info" : savedSearch,
+                   "custom_meta_labels" : [],
+                   "custom_classification" : ""
+                   }
+        
+        objectList.append(dbEntry)
+
+def getApps(hostname, host, port, user, password, objectList):
+    requestApps = host + ":" + port + "/services/apps/local&f=table&f=label&f=title&f=version&f=description"
+
+    appsParams = {"count": "0",
+                    "search": "disabled=0"}
+    response = requests.get(url=requestApps, auth=(user, password), data=body, verify=False, params=appsParams).json()
+
+    apps = response["entry"]
+    
+    for app in apps:
+        dbEntry = {"splunk_host" : hostname,
+                   "type": "Dashboard",
+                   "object_info" : app,
                    "custom_meta_labels" : [],
                    "custom_classification" : ""
                    }
