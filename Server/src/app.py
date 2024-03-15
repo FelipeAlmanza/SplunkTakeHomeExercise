e4a615b2cd2af88b1fb0e4e51916c3a6b50c9805
felipealmanza <f.almanza4@gmail.com>
2024-03-15 00:39:36 -0600

    Added overview function

--- Server/src/app.py
+++ Server/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from collections import defaultdict
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 import yaml
 
@@ -14,6 +15,32 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     return 'Splunk Take Home Exercise REST API'
+
+@app.route('/overview', methods=["GET"])
+def get_overview():
+    objects = mongo.db[dbConfig["collection"]].find()
+
+    # Counts ocurrences of type values
+    objectCounter = defaultdict(int)
+    for object in objects:
+        objectCounter[object["type"]] += 1
+
+    objectCounterDic = dict(objectCounter)
+
+    response = jsonify({"Overview" : objectCounterDic})
+
+    return response
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    response = jsonify({
+        "message" : "Resource not found: " + request.url,
+        "status" : 404
+    })
+    response.status_code = 404
+    return response
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
